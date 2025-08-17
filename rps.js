e581250ddb96e328652f4c628d681cfc950598b5
Cmwilkerson <216974678+cmwilkerson@users.noreply.github.com>
2025-08-17 10:49:55 -0700

    Built computer choice logic

--- rps.js
+++ rps.js
@@ -1,1 +1,9 @@
 console.log("Hello from the Javaside")
+function getComputerChoice(){
+    let rock=Math.random();
+    let paper=Math.random();
+    let scissors=Math.random();
+    
+    return rock > paper? (rock > scissors? "rock": "scissors"):(paper>scissors? "paper":"scissors")
+}
+console.log(getComputerChoice())
